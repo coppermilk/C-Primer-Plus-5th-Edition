cbacd9528f5ff8842c4b227898beafd3c6ff1bf3
coppermilk <art@coppermilk.org>
2021-07-28 22:53:45 +0300

    Create ch02ex02.cpp

---
+++ ch2/ch02ex02.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+        double furlongs;
+
+        cout << "Entering the distance in furlongs: ";
+        cin >> furlongs;
+        cout << "This distance is equivalent to " << furlongs * 220 << " yards";
+
+        return 0;
+}

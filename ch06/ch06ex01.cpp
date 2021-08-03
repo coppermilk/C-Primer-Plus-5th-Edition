c30ba0f6a959463835fc65aeae1416a9a7b657e2
coppermilk <nataliyakowtun67@gmail.com>
2021-08-03 09:39:17 +0300

    Create ch06ex01.cpp

---
+++ ch06/ch06ex01.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cctype>
+
+using namespace std;
+
+int main() {
+        char ch;
+        cin.get(ch);
+        
+        while (ch != '@') {
+                if (!isdigit(ch)) {
+                    
+                        if (islower(ch)) {
+                                ch = toupper(ch);
+                                
+                        } else if (isupper(ch)) {
+                                ch = tolower(ch);
+                        }
+                        cout << ch;
+                }
+
+                cin.get(ch);
+        }
+
+        return 0;
+}

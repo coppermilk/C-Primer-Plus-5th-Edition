a072ce2669c75ce292e9f3427a6ca7c510da6bfe
coppermilk <nataliyakowtun67@gmail.com>
2021-08-03 13:35:54 +0300

    Create ch04ex07.cpp

---
+++ ch04/ch04ex07.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+struct pizza {
+        char vendor[80];
+        float diameter;
+        float weight;
+};
+
+int main() {
+        pizza first_pizza;
+        cout << "Enter pizza vendor: ";
+        cin.getline(first_pizza.vendor, 80);
+        cout << "Enter pizza diameter: ";
+        cin >> first_pizza.diameter;
+        cout << "Enter pizza weight";
+        cin >> first_pizza.weight;
+
+        cout << endl;
+        cout << "Vendor: " << first_pizza.vendor << endl;
+        cout << "Diameter: " << first_pizza.diameter << endl;
+        cout << "Weight: " << first_pizza.weight << endl;
+
+        return 0;
+}

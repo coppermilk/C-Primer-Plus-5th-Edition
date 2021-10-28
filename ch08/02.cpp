17ec6a0931e964837bfe419c982bf9d02dc4d7fc
coppermilk <nataliyakowtun67@gmail.com>
2021-10-28 23:00:06 +0300

    Create 02.cpp

---
+++ ch08/02.cpp
@@ -0,0 +1,35 @@
+// 8.2
+//
+#pragma warning(disable: 4996)
+#include <iostream>
+
+struct CandyBar {
+        char name[64];
+        double weight;
+        int callories;
+};
+
+void fill(CandyBar & c,
+        const char * n = "Millennium Munch", double w = 2.85, int cal = 350);
+        
+void show(CandyBar & c);
+
+int main() {
+        CandyBar candy;
+        fill(candy);
+        show(candy);
+        fill(candy, "Roshen", 5.68, 670);
+        show(candy);
+
+}
+
+void fill(CandyBar & c,
+        const char * n, double w, int cal) {
+        strcpy(c.name, n);
+        c.weight = w;
+        c.callories = cal;
+}
+
+void show(CandyBar & c) {
+        std::cout << c.name << " " << c.weight << " " << c.callories << std::endl;
+}

00de1f5999a86675545f072c2258088acb54074f
coppermilk <nataliyakowtun67@gmail.com>
2021-10-29 00:03:27 +0300

    Update 03.cpp

--- ch08/03.cpp
+++ ch08/03.cpp
@@ -16,6 +16,7 @@ int main() {
         return 0;
 }
 
+// this function takes a string as a constant argument, and convert letters to uppercase
 void show(const string str) {
         for (int i = 0; str[i]; i++) {
                 cout << (char) toupper(str[i]);

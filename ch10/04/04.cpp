f1517f908e2e45bd68e053ebf756e39cbfbe378e
coppermilk <nataliyakowtun67@gmail.com>
2021-11-09 22:43:14 +0300

    Update 04.cpp

--- ch10/04/04.cpp
+++ ch10/04/04.cpp
@@ -1,65 +1,22 @@
-#include "firstnamespace.h"
 #include <iostream>
-// void SALES::Sales::
- SALES::Sales::Sales(const double ar[], int n) {
-    for (int i = 0; i < n; i++) {
-        sales[i] = ar[i];
+#include "firstnamespace.h"
 
-    }
-    average = getAverage(ar, n);
-    max = getMax(ar, n);
-    min = getMin(ar, n);
-}
- 
- SALES::Sales::Sales() {
-    for (int i = 0; i < SALES::Sales::QUARTERS; i++) {
-        std::cout << "Enter total sales for " << i + 1 << " quarters: ";
-        std::cin >> sales[i];
+using namespace std;
 
-    }
-    average = getAverage(sales, SALES::Sales::QUARTERS);
-    max = getMax(sales, SALES::Sales::QUARTERS);
-    min = getMin(sales, SALES::Sales::QUARTERS);
-}
+int main() {
 
+    const double ar[] = {
+            16,
+            32,
+            64,
+            128
+    };
+    
+    SALES::Sales s1(ar, 4);
+    s1.showSales();
 
-void SALES::Sales::showSales() {
+    SALES::Sales s2;
+    s2.showSales();
 
-    for (int i = 0; i < SALES::Sales::QUARTERS; i++) {
-        std::cout << "Enter total sales for " << i + 1 << " quarters: " << sales[i] << std::endl;
-    }
-
-    std::cout << "average: " << average << std::endl;
-    std::cout << "max: " << max << std::endl;
-    std::cout << "min: " << min << std::endl;
-
-}
-
-double SALES::Sales::getAverage(const double ar[], int n) {
-    double sum = 0;
-    for (int i = 0; i < SALES::Sales::QUARTERS; i++) {
-        sum += ar[i];
-    }
-    return sum / SALES::Sales::QUARTERS;
-
-}
-
-double SALES::Sales::getMax(const double ar[], int n) {
-    double max = ar[0];
-    for (int i = 0; i < SALES::Sales::QUARTERS; i++) {
-        if (max < ar[i]) {
-            max = ar[i];
-        }
-    }
-    return max;
-}
-
-double SALES::Sales::getMin(const double ar[], int n) {
-    double min = ar[0];
-    for (int i = 0; i < SALES::Sales::QUARTERS; i++) {
-        if (min > ar[i]) {
-            min = ar[i];
-        }
-    }
-    return min;
+    return 0;
 }

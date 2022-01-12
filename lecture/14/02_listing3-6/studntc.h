a781246c22d3aa683f4fa49d78b4d0e4d0ace6df
Mr. Coppermilk <nataliyakowtun67@gmail.com>
2022-01-12 17:57:20 +0300

    Add files via upload

---
+++ lecture/14/02_listing3-6/studntc.h
@@ -0,0 +1,35 @@
+#ifndef STUDENTC_H_
+#define STUDENTC_H_
+#include <iostream>
+#include <string>
+#include <valarray>
+
+
+class Student :private std::string, private std::valarray<double> {
+private:
+	typedef std::valarray<double> ArrayDb;
+	//std::string name;
+	//ArrayDb scores;
+
+	std::ostream & arr_out(std::ostream & os) const;
+public:
+	Student() : std::string("Null Student"), ArrayDb() { }
+	Student(const std::string& s) :std::string(s), ArrayDb() { }
+	Student(int n) : std::string("Nully"), ArrayDb(n) { }
+	Student(const std::string& s, int n) : std::string(s), ArrayDb(n) {}
+	Student(const std::string& s, const ArrayDb& a) : std::string(s), ArrayDb(a) {}
+	Student(const char* str, const double* pd, int n) : std::string(str), ArrayDb(pd, n) {}
+
+	~Student() {}
+
+	double Average() const;
+	const std::string& Name() const;
+	double& operator[] (int i); 
+	double operator[] (int i) const;
+
+	friend std::istream& operator>>(std::istream& is, Student& stu); // 1 слово 
+	friend std::istream& getline(std::istream& is, Student& stu); // 1 строка 
+	friend std::ostream& operator<<(std::ostream& os, const Student& stu);
+
+};
+#endif //STUDENTC_H_

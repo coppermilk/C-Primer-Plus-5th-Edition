1524bfe1aae9e06833c4e47e21534bcf1a3fe14a
Mr. Coppermilk <nataliyakowtun67@gmail.com>
2022-01-21 21:11:44 +0300

    Create main.cc

---
+++ ch14/01/main.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "tv.h"
+
+int main()
+{
+	using std::cout;
+	Tv s27;
+	cout << "Factory settings\n";
+	s27.settings();
+	s27.onoff();
+	s27.chanup();
+	cout << "\nCanged setting TV with 27 diagonal\n";
+	s27.settings(); 
+	Remote gray;
+	gray.set_chan(s27, 10);
+	gray.volup(s27);
+	gray.volup(s27);
+	cout << "\nChanged setting TV with 27 diagonal\n";
+	s27.toggleRemoteMode(gray);
+	s27.settings();
+	Tv s32(Tv::On);
+	s32.set_mode();
+	gray.set_chan(s32, 28);
+	cout << "\nChanged setting TV with 27 diagonal\n";
+	s32.settings();
+
+}

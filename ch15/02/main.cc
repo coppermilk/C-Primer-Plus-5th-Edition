871ef5c5e5a3073a91f19d4cde699ce11d9b11a7
Mr. Coppermilk <nataliyakowtun67@gmail.com>
2022-01-21 21:30:04 +0300

    Create main.cc

---
+++ ch15/02/main.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cmath>
+#include "exc_mean.h"
+
+double hmean(double a, double b) throw (bad_hmean);
+double gmean(double a, double b) throw (bad_gmean);
+
+int main()
+{
+	using std::cout;
+	using std::cin;
+	using std::endl;
+
+	double x, y, z;
+	cout << "Input two numbers: ";
+	while (cin >> x >> y) {
+		try {
+			z = hmean(x, y);
+			cout << "Harmony: " << z << endl;
+			cout << "Geometry: " << gmean(x, y) << endl;
+		}
+		catch (bad_hmean& bg) {
+			cout << bg.what();
+			/*bg.mesg();
+			cout << "Repet input.\n";*/
+			continue;
+		}
+		catch (bad_gmean& hg) {
+			cout << hg.what();
+		/*	cout << hg.mesg();
+			cout << "Sorry try is enof: " << hg.v1 << ", " << hg.v2 << endl;*/
+			break;
+		}
+	}
+}
+
+double hmean(double a, double b) throw(bad_hmean) {
+	if (a == -b) {
+		throw bad_hmean(a, b);
+	}
+	return 2.0 * a * b / (a + b);
+
+}
+double gmean(double a, double b) throw(bad_gmean) {
+	if (a < 0 || b < 0) {
+		throw bad_gmean(a, b);
+	}
+	return std::sqrt(a * b);
+}
